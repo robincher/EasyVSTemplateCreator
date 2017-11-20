4493f25633fb143e72c7cfeafc7657c0e3162e41
Ng Sze Kern <rebooting@gmail.com>
2017-11-20 12:22:42 +0800

    added rust template

--- src/main.go
+++ src/main.go
@@ -18,93 +18,146 @@ Copyrights 2017 Alvin Ng
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 */
-import(
+import (
 	"fmt"
-	"os/exec"
 	"os"
-	"unicode"
+	"os/exec"
+	"strings"
 	"time"
+	"unicode"
 )
 
-func main(){
+func main() {
 	args := os.Args
-	if len(args)>1{
+
+	if len(args) > 2 {
 		var newProjectName = args[1]
 		log("deleting any existing template directory...")
-		deleteExistingDirectory()
-		if isAlphanumeric(newProjectName){
-			log("cloning github template")
-			if(pullRepo()){
-				log("cloning done, renaming")
-				//allow a pause as I've encountered directory lock in windows
-				time.Sleep(time.Millisecond*200)
-				log("removing git")
-				purgeExistingGitDirectory()
-				renameRepo(newProjectName)
-				log("renaming done")
-			}else{
-				log("error occured")
+
+		if isAlphanumeric(newProjectName) {
+			if strings.ToLower(args[2]) == "go" {
+				createGoProject(newProjectName)
+			} else if strings.ToLower(args[2]) == "rust" {
+				createRustProject(newProjectName)
+			} else {
+				printHelp()
 			}
-			
-		}else{
-			fmt.Println("please use alphanumeric characters for project folder")
+		} else {
+			printHelp()
 		}
+	} else {
+		printHelp()
 	}
-	
+
 }
 
-func pullRepo() bool{
+func createRustProject(newProjectName string) {
+	deleteExistingRustDirectory()
+	log("cloning github template")
+	if pullRustRepo() {
+		log("cloning done, renaming")
+		//allow a pause as I've encountered directory lock in windows
+		time.Sleep(time.Millisecond * 200)
+		log("removing git")
+		purgeExistingGitDirectory()
+		renameRepo("RustVSCodeTemplate", newProjectName)
+		log("renaming done")
+	} else {
+		log("error occured")
+	}
+
+}
+
+func createGoProject(newProjectName string) {
+	deleteExistingGoDirectory()
+	log("cloning github template")
+	if pullGoRepo() {
+		log("cloning done, renaming")
+		//allow a pause as I've encountered directory lock in windows
+		time.Sleep(time.Millisecond * 200)
+		log("removing git")
+		purgeExistingGitDirectory()
+		renameRepo("VSCodeGoLangStarterTemplate", newProjectName)
+		log("renaming done")
+	} else {
+		log("error occured")
+	}
+
+}
+
+func printHelp() {
+	fmt.Println("easycreate <project> <go/rust>")
+}
+func pullGoRepo() bool {
 	var cmd = "git"
-	var args = []string{"clone","https://github.com/RoteErde/VSCodeGoLangStarterTemplate"}
-	if cmdout, err := exec.Command(cmd, args...).Output(); err !=nil{
+	var args = []string{"clone", "https://github.com/RoteErde/VSCodeGoLangStarterTemplate"}
+	if cmdout, err := exec.Command(cmd, args...).Output(); err != nil {
 		log(string(cmdout))
 		log(err.Error())
 		return false
-		}
-	
+	}
+
 	return true
 }
 
-func renameRepo(newName string) bool{
-	
-	if err := os.Rename("VSCodeGoLangStarterTemplate", newName); err !=nil{
-		log("failed to rename")
+func pullRustRepo() bool {
+	var cmd = "git"
+	var args = []string{"clone", "https://github.com/RoteErde/RustVSCodeTemplate"}
+	if cmdout, err := exec.Command(cmd, args...).Output(); err != nil {
+		log(string(cmdout))
 		log(err.Error())
-		return false;
+		return false
 	}
-	
-	return true;
+
+	return true
 }
 
+func renameRepo(templateName string, newName string) bool {
+
+	//if err := os.Rename("VSCodeGoLangStarterTemplate", newName); err != nil {
+	if err := os.Rename(templateName, newName); err != nil {
+		log("failed to rename")
+		log(err.Error())
+		return false
+	}
+
+	return true
+}
 
 /*
 	returns true if character is either alphabet or number
 	we don't want folders with funny characters
 */
-func isAlphanumeric(foldername string) bool{
-	for _, character := range foldername{
-		if !unicode.IsDigit(character) && !unicode.IsLetter(character){
+func isAlphanumeric(foldername string) bool {
+	for _, character := range foldername {
+		if !unicode.IsDigit(character) && !unicode.IsLetter(character) {
 			return false
 		}
 	}
 	return true
 }
 
-
 /*
 remove current cached directory (in case previous operations failed)
 */
-func deleteExistingDirectory()bool{
-	if err := os.RemoveAll("VSCodeGoLangStarterTemplate"); err!=nil{
+func deleteExistingGoDirectory() bool {
+	if err := os.RemoveAll("VSCodeGoLangStarterTemplate"); err != nil {
 		log(err.Error())
 		return false
 	}
 	return true
 }
 
+func deleteExistingRustDirectory() bool {
+	if err := os.RemoveAll("RustVSCodeTemplate"); err != nil {
+		log(err.Error())
+		return false
+	}
+	return true
+}
 
-func purgeExistingGitDirectory()bool{
-	if err := os.RemoveAll("VSCodeGoLangStarterTemplate/.git"); err!=nil{
+func purgeExistingGitDirectory() bool {
+	if err := os.RemoveAll("VSCodeGoLangStarterTemplate/.git"); err != nil {
 		log(err.Error())
 		return false
 	}
